f516b8d49d2775460561d2e41a27c96f512fb5a2
cersosimovicente <128232790+cersosimovicente@users.noreply.github.com>
2024-08-07 16:19:20 -0300

    Update datos.js

--- proyectos/proyecto2/datos.js
+++ proyectos/proyecto2/datos.js
@@ -1,1 +1,26 @@
+let products = [
+  {
+    id: 1,
+    nombre: "Cerradura",
+    descripcion: "Cerradura Doble perno",
+    precio: 100,
+    cantidad: 10,
+  },
+  {
+    id: 2,
+    nombre: "Candado",
+    descripcion: "Candado Acero laminado",
+    precio: 200,
+    cantidad: 20,
+  },
+  {
+    id: 3,
+    nombre: "Cerrojo",
+    descripcion: "Cerrojo para porton",
+    precio: 300,
+    cantidad: 30,
+  },
+];
+
+export default products;
 
